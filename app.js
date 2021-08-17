4538e8444063147740c38f1da3bd8899ebedfa71
Patrick Mulligan <pat@pop-os.localdomain>
2021-08-17 13:58:19 -0600

    Step 5 Create a Router which takes the responsibility of listing out our app's different routes - Create router.js file 	-export express.Router() variable ( `module.exporte = router`) - Require it into the app.js file 	- include `app.use('/', router)` in app.js - Move all routes to the router file, e.g., '/' and '/about'

--- app.js
+++ app.js
@@ -3,14 +3,11 @@ const express = require('express') // const doesn't allow you to re-assign a val
 const app = express()
 
 const router = require('./router.js') // .js not necessary at end of file
-console.log(router)
 
 app.use(express.static('public'))
 app.set('views','views') // first arg 'views' is an express option, second arg is the folder name we've chosen
 app.set('view engine', 'ejs') // pug and handlebars are other options of templates with their own syntax
 
-app.get('/', function(req, res) {
-    res.render('home-guest')
-})
+app.use('/', router) // 
 
 app.listen(4000)
