1f8482eee45d7cfa551cbdd1823e9ea759f21345
Patrick Mulligan <pat@pop-os.localdomain>
2021-08-17 13:47:33 -0600

    This is a small demonstration of how importing another self-created js file works
    We know we can use `let var_name = require('<package>') to import a
    package
    
    We can also do this to import our own files
    `let var_name = require('./path/to/file')`
    
    Note that this both:
    1) executes the file, e..g, the console.log() inside of router.js
    2) exports whatever you specify with module.exportsin the router.js file and stores it in the variable var_name for use later
    	- you can export strings, numbers, arrays, or most commonly
    	  objects
    	- with objects then you have access their properties, methods,
    	  etc

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express') // const doesn't allow you to re-assign a val
 
 const app = express()
 
+const router = require('./router.js') // .js not necessary at end of file
+console.log(router)
+
 app.use(express.static('public'))
 app.set('views','views') // first arg 'views' is an express option, second arg is the folder name we've chosen
 app.set('view engine', 'ejs') // pug and handlebars are other options of templates with their own syntax
