871d74d782e7cd71d0ad7173fb262a19ffd67463
Patrick Mulligan <pat@pop-os.localdomain>
2021-08-17 13:12:57 -0600

    Step 3 - add public folder for css, js files - include `app.use(express.static('public')) in app.js - retrieve css file and modify html with / (/ not needed if you were just to dl the zip and click on files, but needed in the context of this application)

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express') // const doesn't allow you to re-assign a val
 
 const app = express()
 
+app.use(express.static('public'))
 app.set('views','views') // first arg 'views' is an express option, second arg is the folder name we've chosen
 app.set('view engine', 'ejs') // pug and handlebars are other options of templates with their own syntax
 
