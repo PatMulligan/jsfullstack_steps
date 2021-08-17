857664d9a0e8a714d1d7b80a4444c9b1e9e4c0cb
Patrick Mulligan <pat@pop-os.localdomain>
2021-08-17 13:05:13 -0600

    Step 2 - create 'views' folder (could have named it anything) - create .ejs file in views and use ejs template engine - npm install ejs - set appropriate values and render the ejs file webpage

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express') // const doesn't allow you to re-assign a val
 
 const app = express()
 
+app.set('views','views') // first arg 'views' is an express option, second arg is the folder name we've chosen
+app.set('view engine', 'ejs') // pug and handlebars are other options of templates with their own syntax
+
 app.get('/', function(req, res) {
-    res.send("Welcome to your new app")
+    res.render('home-guest')
 })
 
 app.listen(4000)
