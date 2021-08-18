5135067acde1c4f2e08d38a081a8817c062fccc0
Patrick Mulligan <patjmulligan@protonmail.com>
2021-08-18 08:15:33 -0600

    Step 8b pass data from the browser to the constructor to create unique new user - in the User() constructor of models/User.js, accept an argument, e.g.,   "data" 	- let User = function(data) {...} - pass `req.body` data to the constructor; `new User(req.body)` in   userController.js - in this example, we then call the register fct in userController.js   which has the "business" logic / rules we want to enforce for the model

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,7 +14,9 @@ exports.logout = function() {
 }
 
 exports.register = function(req, res) {
-    let user = new User() // create a new object of a User using the constructor
+    let user = new User(req.body) // create a new object of a User using the constructor
+    console.log(user.data) // this property is created in the User() constructor of User.js
+    user.register()
     res.send("Thanks for trying to register")
 }
 
--- models/User.js
+++ models/User.js
@@ -1,18 +1,25 @@
 // Lec. 49 at 7m
-let User = function() {
+let User = function(data) {
     // when calling the constructor we use `user = new User()`
     // the new keyword creates an empty object named user, from which the constructor is being called
     // therefore the `this` keywork here points to that object, essentially saying user.homePlante = "earth"
-    this.homePlanet = "earth"
+    //this.homePlanet = "earth"
+
+    this.data = data //note the variable name to store data can be whatever
 
     // This is the trivial way of adding a method to a constructor
     // better to use the <Constructur>.prototype.<method name> below
-    this.jump = function() {} //remove this in favor of User.prototype.jump
+    //this.jump = function() {} //remove this in favor of User.prototype.jump
 }
 
 // more efficient way to add a method to a constructor
 // this way the fct doens't get stored in memory for every instance of the Model created
-User.prototype.jump = function() {}
+//User.prototype.jump = function() {}
+
+User.prototype.register = function() {
+    // enforce all of our business logic here
+    console.log("new user registered")
+}
 
 // to be able to leverage this function from our userController file:
 module.exports = User
